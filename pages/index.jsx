50e2e9ceb2ff68dfbcd55483827e96848ef5ca96
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-03-23 15:37:03 +0100

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div>
         <h1>World Trade Center</h1>
         <img
-          src="https://maps.googleapis.com/maps/api/staticmap?center=48.8584,2.294694&zoom=16&size=400x400&key=AIzaSyDxWgsx_YbvSPhJBY9s-FoyqlODjyN8Zg8&markers=color:red|label:S|48.8587,2.293710"
+          src="https://maps.googleapis.com/maps/api/staticmap?center=48.8584,2.294694&zoom=16&size=400x400&key=${Your API key}&markers=color:red|label:S|48.8587,2.293710"
           alt="World Trade Center"
         />
       </div>
