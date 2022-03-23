58320b307c148011c4eb5f458fba48ed3b4ab1de
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-03-23 15:34:51 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>World Trade Center</h1>
+        <img
+          src="https://maps.googleapis.com/maps/api/staticmap?center=48.8584,2.294694&zoom=16&size=400x400&key=AIzaSyDxWgsx_YbvSPhJBY9s-FoyqlODjyN8Zg8&markers=color:red|label:S|48.8587,2.293710"
+          alt="World Trade Center"
+        />
+      </div>
+    </div>
+  );
+}
