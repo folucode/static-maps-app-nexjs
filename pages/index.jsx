b62d86803012be7d2d10673f769b448b6eea9fda
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-03-24 12:57:57 +0100

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <div>
-        <h1>World Trade Center</h1>
-        <img
-          src="https://maps.googleapis.com/maps/api/staticmap?center=48.8584,2.294694&zoom=16&size=400x400&key=${Your API key}&markers=color:red|label:S|48.8587,2.293710"
-          alt="World Trade Center"
-        />
-      </div>
+      <h1>A Static Map Image of the Eiffel Tower!</h1>
+      <img
+        src='https://maps.googleapis.com/maps/api/staticmap?center=48.8584,2.294694&zoom=16&size=400x400&key=${Your_api_key}&markers=color:red|label:S|48.8587,2.293710'
+
+        alt='World Trade Center'
+      />
     </div>
   );
 }
